850bdcb4cbe959c8867edeeec0f252351b8961d1
league <league@imac.attlocal.net>
2023-01-21 16:29:06 -0800

    2nd day of level 1

--- src/_01_methods/_1_houses/Houses.java
+++ src/_01_methods/_1_houses/Houses.java
@@ -1,4 +1,7 @@
 package _01_methods._1_houses;
+
+import org.jointheleague.graphical.robot.Robot;
+
 /*
  *    Copyright (c) The League of Amazing Programmers 2013-2021
  *    Level 1
@@ -7,7 +10,15 @@ public class Houses {
 		
 	public void run() {
 		// Check the recipe to find out what code to put here
-		Robot.Rob = new Robot
+		Robot Rob=new Robot();
+		Rob.setX(800);
+		Rob.penDown();
+		Houses houseBuilder = new Houses();
+		houseBuilder.run();
+		
+				
+		
+		
 			
 	}
 }
