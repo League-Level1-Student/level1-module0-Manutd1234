79da3676c23445c60395aa63a6081715ffdc8694
league <league@imac.attlocal.net>
2023-05-18 19:59:39 -0700

    Week 12

--- src/_01_methods/_5_FlappyBird/FlappyBird.java
+++ src/_01_methods/_5_FlappyBird/FlappyBird.java
@@ -5,7 +5,10 @@ import processing.core.PApplet;
 public class FlappyBird extends PApplet {
     static final int HEIGHT = 600;
 	static final int WIDTH = 800;
-
+	int x = 100;
+	int y = 100;
+	int birdVelocity = -10;
+	int gravity = 1; 
     @Override
     public void settings() {
     	size(WIDTH,HEIGHT);  
@@ -18,14 +21,20 @@ public class FlappyBird extends PApplet {
 
     @Override
     public void draw() {
-    	background(0, 0, 50);
-    	fill(0, 0, 50);
-    	stroke(0, 0, 10);
-    	ellipse(100 , 100, WIDTH, HEIGHT);
-    	int birdVelocity = -10;
-    	int gravity = 1;
-    }
+    	background(185, 220, 238);
+    	fill(185, 220, 238);
+    	noStroke();
+    	ellipse( x,y , WIDTH, HEIGHT);
+    	fill(185,220,238);
+    	rect(x, y, width, height);
 
+    	
+    }
+    
+    @Override
+    public void mousePressed() {
+    	
+    }
     static public void main(String[] args) {
         PApplet.main(FlappyBird.class.getName());
     }
