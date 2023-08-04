c88b1e96f6c82b3a32a8e2685e9ea048122f1d2b
league <league@imac-14.attlocal.net>
2023-08-03 19:56:31 -0700

    week 14

--- src/_02_return_types/_1_finish_line/FinishLine.java
+++ src/_02_return_types/_1_finish_line/FinishLine.java
@@ -17,16 +17,19 @@ public class FinishLine {
 	
 	public static void main(String[] args) {
 		//1. Call the drawFinishLine() method
-		
+		drawFinishLine();
 		//2. Call the crazyMove() method to move the robot
-		
+		crazyMove();
 		//3. Call the getTotalDistance() and save what is returned into a variable
-		
+		int Distance = getTotalDistance();
 		//4. Call the hasCrossedFinishLine() method and save what is returned into a variable
-		
+		Boolean hasFinished = hasCrossedFinishLine();
 		//5. If the robot has crossed the finish line... 
-			
+			if(hasFinished) {
+				
+			}
 			//6. Use a pop up to say the robot finished and how far it went
+		// String input = JOptionPane.showInputDialog("The robot has finished and went )
 		
 		
 		//7. Else use a pop up to say the robot did not finish and how far it went
@@ -78,7 +81,7 @@ public class FinishLine {
 			return true;
 		}
 		return false;
-		JOption.pane.showInputDialog(Robot has finished it went )
+		
 	}
 	
 }
