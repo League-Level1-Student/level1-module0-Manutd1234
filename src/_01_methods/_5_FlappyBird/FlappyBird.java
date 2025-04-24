dc605c40ad2f12300cc55cdfcc49d0c0aa4cf6c2
opie10 <augie@keatinge.com>
2025-04-23 19:58:51 -0700

    fcy

--- src/_01_methods/_5_FlappyBird/FlappyBird.java
+++ src/_01_methods/_5_FlappyBird/FlappyBird.java
@@ -7,9 +7,12 @@ public class FlappyBird extends PApplet {
     static final int HEIGHT = 600;
     int x =20;
     int y =20;
-    int speed = 5;
     int birdYVelocity=-10;
    	int gravity = 1;
+   	int pipeXPosition = 800;
+   	int upperPipeHeight = (int) random(100, 400);
+   	int pipeGap = 40;
+   	int lowerY = (int) random(100, 400);
     @Override
     public void settings() {
     	size(WIDTH, HEIGHT);
@@ -17,22 +20,39 @@ public class FlappyBird extends PApplet {
 
     @Override
     public void setup() {
-    	 
+ 
     }
 
-    @Override
+    
+		
+
+	@Override
     public void draw() {
     	background(0, 250, 40);
     	fill(0, 0, 0);
     	stroke(0, 0, 0);
     	ellipse(x,y , 20,20);
-    	y+=speed;	
+    	y+=birdYVelocity;	
     	fill(0, 255, 0);
-    	rect(x, y, 40, 50);
+    	rect(pipeXPosition, 0, 60, upperPipeHeight);
+    	birdYVelocity+=gravity;
+    	pipeXPosition-=3;
+  teleportPipes();
+  lowerY = upperPipeHeight + pipeGap;
+
+
     }
+    @Override
     public void mousePressed() {
-    	int resetBirdYVelocity = 3;
+    	 birdYVelocity= -10;
     }
+   public void teleportPipes() {
+	   if(pipeXPosition<-60) {
+		  pipeXPosition = 800;
+		upperPipeHeight = (int) random(100,400);
+	 }
+	   
+   }
 
     static public void main(String[] args) {
         PApplet.main(FlappyBird.class.getName());
